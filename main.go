02f380f3906eea9b2b7409612be72eb436f250de
basicFlutter <farshid3745@gmail.com>
2023-12-28 18:28:35 +0330

    second commit

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	// "context"
-	"fmt"
 	"grpc_test/chat"
 	"log"
 	"net"
@@ -14,9 +13,8 @@ import (
 
 func main() {
 
-	fmt.Println("Go gRPC Beginners Tutorial!")
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -27,6 +25,7 @@ func main() {
 
 	chat.RegisterChatServiceServer(grpcServer, &s)
 
+	log.Println("listening on port 9000")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
